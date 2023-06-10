15c67c817b7e98739fb6f484a1b1394be818ce3e
cshimm <dameepman@gmail.com>
2023-06-10 13:21:30 -0700

    fixed notification builder

--- app/src/main/java/com/example/scatracker/MainActivity.kt
+++ app/src/main/java/com/example/scatracker/MainActivity.kt
@@ -25,10 +25,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //define network constraints to require internet connection
         val networkConstraints = Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED)
             .build()
+        //define cat agent
         val catAgentId = "CatAgent1"
 
+        //define all the tasks that will be requested of the cat agent
+        //set their network constraints and input data
         val catStretchingRequest = OneTimeWorkRequest.Builder(CatStretchingWorker::class.java)
             .setConstraints(networkConstraints)
             .setInputData(
@@ -50,11 +54,13 @@ class MainActivity : AppCompatActivity() {
                 getCatAgentIdInputData(CatSuitUpWorker.INPUT_DATA_CAT_AGENT_ID, catAgentId)
             ).build()
 
+        //set observers for all requests and set success message
         observeRequest(catStretchingRequest, "Agent done stretching")
         observeRequest(catFurGroomingRequest, "Agent done grooming its fur")
         observeRequest(catLitterBoxRequest, "Agent done with the litter box")
         observeRequest(catSuitUpRequest, "Agent done suiting up. Ready to go!")
 
+        //set the order of requests for the agent
         workManager.beginWith(catStretchingRequest)
             .then(catFurGroomingRequest)
             .then(catLitterBoxRequest)
@@ -62,17 +68,9 @@ class MainActivity : AppCompatActivity() {
             .enqueue()
     }
 
-    private fun getCatSuitUpRequest(
-        networkConstraints: Constraints,
-        catAgentId: String,
-        worker: Worker
-    ) = OneTimeWorkRequest.Builder(worker::class.java)
-        .setConstraints(networkConstraints)
-        .setInputData(
-            getCatAgentIdInputData(CatSuitUpWorker.INPUT_DATA_CAT_AGENT_ID, catAgentId)
-        ).build()
-
+    //set up the observers for agents
     private fun observeRequest(request: OneTimeWorkRequest, resultMessage: String) {
+        //find the work info for the specified id and set the action for when the request is completed
         workManager.getWorkInfoByIdLiveData(request.id)
             .observe(this, Observer { info ->
                 if (info.state.isFinished) {
@@ -82,6 +80,7 @@ class MainActivity : AppCompatActivity() {
             })
     }
 
+    //set observer for tracking service;
     private fun launchTrackingService() {
         RouteTrackingService.trackingCompletion.observe(this, Observer { agentId ->
             showResult("Agent $agentId arrived!")
@@ -89,9 +88,11 @@ class MainActivity : AppCompatActivity() {
         val serviceIntent = Intent(this, RouteTrackingService::class.java).apply {
             putExtra(EXTRA_SECRET_CAT_AGENT_ID, "007")
         }
+        //calls onStartCommand in RouteTrackingService
         ContextCompat.startForegroundService(this, serviceIntent)
     }
 
+    //get the agent's input data to track progress
     private fun getCatAgentIdInputData(catAgentIdKey: String, catAgentIdValue: String) =
         Data.Builder().putString(catAgentIdKey, catAgentIdValue).build()
 
--- app/src/main/java/com/example/scatracker/RouteTrackingService.kt
+++ app/src/main/java/com/example/scatracker/RouteTrackingService.kt
@@ -11,54 +11,88 @@ import android.os.Handler
 import android.os.HandlerThread
 import android.os.IBinder
 import android.os.Looper
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.scatracker.worker.makeStatusNotification
 
+@Suppress("DEPRECATION")
 class RouteTrackingService : Service() {
+    //Get reference to the notification builder and service handler
     private lateinit var notificationBuilder: NotificationCompat.Builder
     private lateinit var serviceHandler: Handler
 
     override fun onCreate() {
         super.onCreate()
+        //populate notificationBuilder from the return value of startForegroundService
         notificationBuilder = startForegroundService()
+        //define and start the thread for concurrency
         val handlerThread = HandlerThread("RouteTracking").apply { start() }
+        //populate serviceHandler with a Handler object using the handlerThread
         serviceHandler = Handler(handlerThread.looper)
     }
-
-    companion object {
-        const val NOTIFICATION_ID = 0xCA7
-        const val EXTRA_SECRET_CAT_AGENT_ID = "scaId"
-        private val mutableTrackingCompletion = MutableLiveData<String>()
-        val trackingCompletion: LiveData<String> = mutableTrackingCompletion
-    }
-    private fun trackToDestination(notificationBuilder: NotificationCompat.Builder) {
-        for (i in 10 downTo 0) {
-            Thread.sleep(1000L)
-            notificationBuilder.setContentText("$i seconds to destination")
-            startForeground(NOTIFICATION_ID, notificationBuilder.build())
+    private fun startForegroundService(): NotificationCompat.Builder {
+        val pendingIntent = getPendingIntent()
+        val channelId = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel()
+        } else {
+            ""
         }
+        //create notificationBuilder with the pending intent and notification channel id
+        val notificationBuilder = getNotificationBuilder(pendingIntent, channelId)
+        //start the foreground service with the notificationBuilder
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+        return notificationBuilder
     }
+
+    //Set up the notification channel with an id and name
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(): String {
+        val channelId = "routeTracking"
+        val channelName = "Route Tracking"
+        val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(channel)
+        return channelId
+    }
+    //add agent id to mutable list of completed agents
     private fun notifyCompletion(agentId: String) {
         Handler(Looper.getMainLooper()).post {
             mutableTrackingCompletion.value = agentId
         }
     }
+    //called from launchTrackingService in MainActivity
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val returnValue = super.onStartCommand(intent, flags, startId)
+        //grab the agent id from the intent string extra, throw error if null
         val agentId = intent?.getStringExtra(EXTRA_SECRET_CAT_AGENT_ID)
             ?: throw IllegalStateException("Agent ID must be provided")
+        //start a runnable service by tracking the agent to destination, setting an observer for completion then stop the command
         serviceHandler.post {
             trackToDestination(notificationBuilder)
             notifyCompletion(agentId)
             stopForeground(true)
             stopSelf()
         }
+        //return the value from the parent call
         return returnValue
     }
+    //tracks cat agent to destination
+    private fun trackToDestination(notificationBuilder: NotificationCompat.Builder) {
+        for (i in 10 downTo 0) {
+            Thread.sleep(1000L)
+            notificationBuilder.setContentText("$i seconds to destination")
+            startForeground(NOTIFICATION_ID, notificationBuilder.build())
 
+            makeStatusNotification("$i seconds to destination", this)
+        }
+    }
+
+    //not using binding so set onBind to null
     override fun onBind(intent: Intent): IBinder? = null
+    //Get the pending intent from the main activity
     private fun getPendingIntent() =
         PendingIntent.getActivity(
             this,
@@ -69,32 +103,21 @@ class RouteTrackingService : Service() {
             ),
             PendingIntent.FLAG_IMMUTABLE
         )
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun createNotificationChannel(): String {
-        val channelId = "routeTracking"
-        val channelName = "Route Tracking"
-        val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
-        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        service.createNotificationChannel(channel)
-        return channelId
-    }
+
+    //Configure the notifications with title, content, icon.
     private fun getNotificationBuilder(pendingIntent: PendingIntent, channelId: String) =
         NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle("Agent approaching destination")
             .setContentText("Agent dispatched")
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentIntent(pendingIntent)
             .setTicker("Agent dispatched, tracking movement")
 
-    private fun startForegroundService(): NotificationCompat.Builder {
-        val pendingIntent = getPendingIntent()
-        val channelId = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            createNotificationChannel()
-        } else {
-            ""
-        }
-        val notificationBuilder = getNotificationBuilder(pendingIntent, channelId)
-        startForeground(NOTIFICATION_ID, notificationBuilder.build())
-        return notificationBuilder
+    //companion object with id and livedata dealing with task completion for easy referencing
+    companion object {
+        const val NOTIFICATION_ID = 0xCA7
+        const val EXTRA_SECRET_CAT_AGENT_ID = "scaId"
+        private val mutableTrackingCompletion = MutableLiveData<String>()
+        val trackingCompletion: LiveData<String> = mutableTrackingCompletion
     }
 }
