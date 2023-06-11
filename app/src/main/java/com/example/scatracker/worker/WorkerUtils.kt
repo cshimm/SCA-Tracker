cf0426b8b4156c9bbd85d1f445a24e71a654b196
cshimm <dameepman@gmail.com>
2023-06-11 15:21:41 -0700

    fixed notification builder

---
+++ app/src/main/java/com/example/scatracker/worker/WorkerUtils.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:JvmName("WorkerUtils")
+
+package com.example.scatracker.worker
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.scatracker.CHANNEL_ID
+import com.example.scatracker.NOTIFICATION_ID
+import com.example.scatracker.NOTIFICATION_TITLE
+import com.example.scatracker.R
+import com.example.scatracker.VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION
+import com.example.scatracker.VERBOSE_NOTIFICATION_CHANNEL_NAME
+
+fun makeStatusNotification(message: String, context: Context) {
+    // Make a channel if necessary
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        val name = VERBOSE_NOTIFICATION_CHANNEL_NAME
+        val description = VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(CHANNEL_ID, name, importance)
+        channel.description = description
+
+        // Add the channel
+        val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+
+        notificationManager?.createNotificationChannel(channel)
+    }
+
+    // Create the notification
+    val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(NOTIFICATION_TITLE)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setVibrate(LongArray(0))
+
+    // Show the notification
+    if (ActivityCompat.checkSelfPermission(
+            context,
+            Manifest.permission.POST_NOTIFICATIONS
+        ) != PackageManager.PERMISSION_GRANTED
+    ) {
+        return
+    }
+    NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, builder.build())
+}
